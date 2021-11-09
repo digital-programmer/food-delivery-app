f02c6318956a465f6a82ba17c73d2428d0686333
digital-programmer <mohandibya123@gmail.com>
2021-11-09 13:21:35 +0530

    Create cart page for empty and filled cart

--- server.js
+++ server.js
@@ -6,17 +6,20 @@ const expressLayout = require("express-ejs-layouts");
 const PORT = process.env.PORT || 8000;
 
 app.use(express.static("public"));
+// Set template engine
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/views"))
+app.set("view engine", "ejs");
 
-app.get("/", (req,res)=> {
+app.get("/", (req, res) => {
     return res.render("home");
 })
 
+app.get('/cart', (req, res) => {
+    return res.render("./customers/cart");
+})
 
-// Set template engine
-app.use(expressLayout);
-app.set("views", path.join(__dirname,"/resources/views"))
-app.set("view engine", "ejs");
 
-app.listen(PORT, (err)=> {
+app.listen(PORT, (err) => {
     console.log(`Server running on port ${PORT}`);
 });
