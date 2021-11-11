2dd87163bfc3293ac5fae1a49242f577a3f4f797
digital-programmer <mohandibya123@gmail.com>
2021-11-11 14:01:28 +0530

    Sessions and cookies setup

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const path = require('path');
 const expressLayout = require("express-ejs-layouts");
 const PORT = process.env.PORT || 8000;
-
 const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('express-flash');
+const MongoDbStore = require('connect-mongo');
+
 
 // Database connection
 const url = "mongodb://localhost/pizza";
@@ -15,6 +19,29 @@ connection.on("error", console.error.bind(console, "Error conecting to MongoDB")
 connection.once("open", () => {
     console.log("Connected to database :: MongoDB");
 })
+
+// Session store
+const mongoStore = MongoDbStore.create({
+    mongoUrl: url,
+    client: connection.getClient(),
+
+}, (err) => {
+    console.log("connect-mongodb setup not ok");
+    console.log(err);
+});
+
+// Session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    },
+    store: mongoStore,
+}))
+
+app.use(flash());
 
 // assets
 app.use(express.static("public"));
@@ -29,4 +56,3 @@ require('./routes/web')(app);
 app.listen(PORT, (err) => {
     console.log(`Server running on port ${PORT}`);
 });
-
